817e24e112cf1660516aadf1b749f6a851fe6f77
shusuke.asami <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2018-03-13 16:04:19 +0000

    kadai: add login system

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
+  
   def index
     @tasks = Task.all.page(params[:page]).per(10)
   end
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     
     if @task.save
       flash[:success] = 'task created'
