6d0ec1aef803257b51aa0fec041619fc211b59ed
shusuke.asami <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2018-03-13 16:35:11 +0000

    for heroku5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   
   def index
@@ -45,14 +45,23 @@ class TasksController < ApplicationController
     flash[:success] = 'task deleted'
     redirect_to tasks_url
   end
-end
 
-private
 
-def set_task
-  @task = Task.find(params[:id])
-end
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 
-def task_params
-  params.require(:task).permit(:content, :status)
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @tasks
+      flash[:danger] = 'you are invalid user'
+      redirect_to root_url
+    end
+  end
 end
