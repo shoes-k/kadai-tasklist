6be3ff3bf0f58476ba7de76eae190429316300c0
shusuke.asami <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2018-03-14 08:52:23 +0000

    retry kadai: add login system

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy, :show]
   
   
   def index
