ad5d233e009e2dde538bfbc8ec1bc537cf89947d
shusuke.asami <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2018-03-15 13:29:40 +0000

    retry_003

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :show, :create, :update, :destroy, ]
+  before_action :correct_user, only: [:edit, :show, :update, :destroy]
   
   
   def index
